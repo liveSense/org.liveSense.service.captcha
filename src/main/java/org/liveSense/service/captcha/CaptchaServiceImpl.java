fae9bb1ce3d20dfa426ade818ac7df38079e6fe2
Robert Csakany <csakany.robert@gmail.com>
2012-11-26 12:57:33 +0100

    Sometimes captcha numbers largers than image. In this case an exception is thrown by JCaptcha. In the case retry to get new image four times.

--- src/main/java/org/liveSense/service/captcha/CaptchaServiceImpl.java
+++ src/main/java/org/liveSense/service/captcha/CaptchaServiceImpl.java
@@ -16,6 +16,7 @@ import org.apache.felix.scr.annotations.Service;
 import org.liveSense.core.Configurator;
 import org.osgi.service.component.ComponentContext;
 
+import com.octo.captcha.service.CaptchaServiceException;
 import com.octo.captcha.service.captchastore.FastHashMapCaptchaStore;
 import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 
@@ -25,107 +26,119 @@ import com.octo.captcha.service.image.DefaultManageableImageCaptchaService;
 @Service
 public class CaptchaServiceImpl implements CaptchaService {
 
-    /**
-     * The value of the parameter indicating the use of a Cookie to store the
-     * authentication data.
-     */
-    private static final String STORAGE_COOKIE = "cookie";
+	/**
+	 * The value of the parameter indicating the use of a Cookie to store the
+	 * authentication data.
+	 */
+	private static final String STORAGE_COOKIE = "cookie";
 
-    /**
-     * The value of the parameter indicating the use of a session attribute to
-     * store the authentication data.
-     */
-    private static final String STORAGE_SESSION_ATTRIBUTE = "session";
+	/**
+	 * The value of the parameter indicating the use of a session attribute to
+	 * store the authentication data.
+	 */
+	private static final String STORAGE_SESSION_ATTRIBUTE = "session";
 
-    /**
-     * To be used to determine if the chalange ID comes from a cookie or from a
-     * session attribute.
-     */
-    private static final String DEFAULT_STORAGE = STORAGE_COOKIE;
-    private static final String PAR_STORAGE = "captcha.storage";
+	/**
+	 * To be used to determine if the chalange ID comes from a cookie or from a
+	 * session attribute.
+	 */
+	private static final String DEFAULT_STORAGE = STORAGE_COOKIE;
+	private static final String PAR_STORAGE = "captcha.storage";
 
-    private static final String PAR_CAPTCHA_ATTRIBUTE_NAME = "captcha.uid";
+	private static final String PAR_CAPTCHA_ATTRIBUTE_NAME = "captcha.uid";
 
-    @Property(name = PAR_STORAGE, value = DEFAULT_STORAGE, options = {
-	    @PropertyOption(name = STORAGE_COOKIE, value = "Cookie"),
-	    @PropertyOption(name = STORAGE_SESSION_ATTRIBUTE, value = "Session Attribute") })
-    private String authStorageType = DEFAULT_STORAGE;
+	@Property(name = PAR_STORAGE, value = DEFAULT_STORAGE, options = {
+			@PropertyOption(name = STORAGE_COOKIE, value = "Cookie"),
+			@PropertyOption(name = STORAGE_SESSION_ATTRIBUTE, value = "Session Attribute") })
+	private final String authStorageType = DEFAULT_STORAGE;
 
-    @Reference
-    Configurator configurator;
+	@Reference
+	Configurator configurator;
 
-    private static DefaultManageableImageCaptchaService instance;
+	private static DefaultManageableImageCaptchaService instance;
 
-    protected void activate(ComponentContext componentContext) {
-	instance = new DefaultManageableImageCaptchaService(
-		new FastHashMapCaptchaStore(), new DefaultGimpyEngine(), 180,
-		100000, 75000);
-    }
+	protected void activate(ComponentContext componentContext) {
+		instance = new DefaultManageableImageCaptchaService(
+				new FastHashMapCaptchaStore(), new DefaultGimpyEngine(), 180,
+				100000, 75000);
+	}
 
-    public BufferedImage getCaptchaImage(String captchaId, Locale locale) {
-	return instance.getImageChallengeForID(captchaId, locale);
-    }
-
-    public boolean validateCapthaResponse(String captchaId, String response) {
-	return instance.validateResponseForID(captchaId, response);
-    }
-
-    public boolean validateCaptchaResponse(HttpServletRequest request, String response) {
-	String captchaId = null;
-	if (authStorageType.equals(STORAGE_COOKIE)) {
-	    captchaId = UUID.randomUUID().toString();
-	    Cookie[] cookies = request.getCookies();
-	    for (int i = 0; i < cookies.length; i++) {
-		Cookie cookie = cookies[i];
-		if (cookie.getName().equals(PAR_CAPTCHA_ATTRIBUTE_NAME)) {
-		    captchaId = cookie.getValue();
+	@Override
+	public BufferedImage getCaptchaImage(String captchaId, Locale locale) {
+		BufferedImage ret = null;
+		for (int i=0; i<3; i++) {
+			try {
+				ret = instance.getImageChallengeForID(captchaId, locale);
+				return ret;
+			} catch (CaptchaServiceException e) {
+			}
 		}
-	    }
-
-	} else {
-	    if (request.getSession() != null) {
-		captchaId = (String)request.getSession().getAttribute(PAR_CAPTCHA_ATTRIBUTE_NAME);
-	    }
+		return instance.getImageChallengeForID(captchaId, locale);
 	}
-	return validateCapthaResponse(captchaId, response);
-    }
 
-    public void setCaptchaId(HttpServletRequest request,
-	    HttpServletResponse response, String captchaId) {
-	if (authStorageType.equals(STORAGE_COOKIE)) {
-	    Cookie[] cookies = request.getCookies();
-	    boolean foundCookie = false;
-
-	    if (cookies != null) {
-        	    for (int i = 0; i < cookies.length; i++) {
-        		Cookie cookie = cookies[i];
-        		if (cookie.getName().equals(PAR_CAPTCHA_ATTRIBUTE_NAME)) {
-        		    foundCookie = true;
-         		    cookie.setMaxAge(configurator.getSessionTimeout().intValue());
-        		    cookie.setValue(captchaId);
-        		    cookie.setPath("/");
-        		    response.addCookie(cookie);
-        		}
-        	    }
-	    }
-
-	    if (!foundCookie) {
-		Cookie cookie = new Cookie(PAR_CAPTCHA_ATTRIBUTE_NAME,
-			captchaId);
-		cookie.setMaxAge(configurator.getSessionTimeout().intValue());
-		cookie.setPath("/");
-		response.addCookie(cookie);
-	    }
-
-	} else {
-	    if (request.getSession() == null) {
-		request.getSession(true).setMaxInactiveInterval(
-			configurator.getSessionTimeout().intValue());
-		request.getSession().setAttribute(PAR_STORAGE, captchaId);
-		captchaId = request.getSession().getId();
-	    }
-
+	@Override
+	public boolean validateCapthaResponse(String captchaId, String response) {
+		return instance.validateResponseForID(captchaId, response);
 	}
-    }
+
+	@Override
+	public boolean validateCaptchaResponse(HttpServletRequest request, String response) {
+		String captchaId = null;
+		if (authStorageType.equals(STORAGE_COOKIE)) {
+			captchaId = UUID.randomUUID().toString();
+			Cookie[] cookies = request.getCookies();
+			for (int i = 0; i < cookies.length; i++) {
+				Cookie cookie = cookies[i];
+				if (cookie.getName().equals(PAR_CAPTCHA_ATTRIBUTE_NAME)) {
+					captchaId = cookie.getValue();
+				}
+			}
+
+		} else {
+			if (request.getSession() != null) {
+				captchaId = (String)request.getSession().getAttribute(PAR_CAPTCHA_ATTRIBUTE_NAME);
+			}
+		}
+		return validateCapthaResponse(captchaId, response);
+	}
+
+	@Override
+	public void setCaptchaId(HttpServletRequest request,
+			HttpServletResponse response, String captchaId) {
+		if (authStorageType.equals(STORAGE_COOKIE)) {
+			Cookie[] cookies = request.getCookies();
+			boolean foundCookie = false;
+
+			if (cookies != null) {
+				for (int i = 0; i < cookies.length; i++) {
+					Cookie cookie = cookies[i];
+					if (cookie.getName().equals(PAR_CAPTCHA_ATTRIBUTE_NAME)) {
+						foundCookie = true;
+						cookie.setMaxAge(configurator.getSessionTimeout().intValue());
+						cookie.setValue(captchaId);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+			}
+
+			if (!foundCookie) {
+				Cookie cookie = new Cookie(PAR_CAPTCHA_ATTRIBUTE_NAME,
+						captchaId);
+				cookie.setMaxAge(configurator.getSessionTimeout().intValue());
+				cookie.setPath("/");
+				response.addCookie(cookie);
+			}
+
+		} else {
+			if (request.getSession() == null) {
+				request.getSession(true).setMaxInactiveInterval(
+						configurator.getSessionTimeout().intValue());
+				request.getSession().setAttribute(PAR_STORAGE, captchaId);
+				captchaId = request.getSession().getId();
+			}
+
+		}
+	}
 
 }
